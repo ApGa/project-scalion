8cd63571b58ab7a9be3651f7302da5ec10734a35
lintang <lintang@sutawika.com>
2025-04-12 15:51:13 -0400

    add task datasets and prompts

--- text-experiments/tasks/text_dataset.py
+++ text-experiments/tasks/text_dataset.py
@@ -2,62 +2,52 @@ import os
 
 from yeval.prompt import YevalPrompt
 from yeval.task import register_task, YevalTask
+from yeval.prompt import register_prompt, YevalPrompt
 
 from yeval.task.commonsense_qa import CommonsenseQATask
 from yeval.task.gsm8k import GSM8KTask
 from yeval.response import extract_answer, get_boxed_answer
 
+def extract_fn(answer: str):
+    try:
+        extracted_answer = answer.split('**Answer:**')[-1].strip()
+        # take numbers only with regex
+        return extracted_answer
+    except Exception as e:
+        return answer
+
+@register_prompt("prompt_cot")
+class CotPrompt(YevalPrompt):
+    user_message="Reason step by step and put your final answer within \\boxed{}."
+    postprocessor=get_boxed_answer
+
+@register_prompt("prompt_cot_answer")
+class CotAnswerPrompt(YevalPrompt):
+    user_message="Reason step by step and put your final answer after \"Answer:\"."
+    postprocessor=extract_fn
+
+@register_task("gsm_symbolic")
+class GSM8KSymbolicTask(YevalTask):
+    data_path="apple/GSM-Symbolic"
+    data_name="p1"
+    input_text=lambda x: x["question"]
+    output_text=lambda x: x["answer"].split("####")[-1].strip()
+    test_split="test"
+    evaluation={"accuracy": lambda x, y: x == y}
+
+@register_task("gsm_symbolic_generate_paraphrase")
+class GSM8KParaphraseTask(GSM8KSymbolicTask):
+    system_message="""You are a helpful paraphrasing model. \
+Write a paraphrase of the question. \
+DO NOT provide the answer.\
+"""
+    sampling_args={"n": 10, "stop": ["Answer:"]}
+
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-@register_task("commonsense_qa_generate_paraphrase")
-class CommonsenseQAParaphraseTask(CommonsenseQATask):
-    system_message="""You are a helpful paraphrasing model. \
-Given a multiple choice question, write a paraphrase of the question-choices. \
-DO NOT provide the answer.\
-"""
-    sampling_args={"n": 10, "stop": ["Answer:"]}
-
-@register_task("gsm8k_generate_paraphrase")
-class GSM8KParaphraseTask(GSM8KTask):
-    system_message="""You are a helpful paraphrasing model. \
-Given a multiple choice question, write a paraphrase of the question-choices. \
-DO NOT provide the answer.\
-"""
-    sampling_args={"n": 10, "stop": ["Answer:"]}
-
-@register_task("commonsense_qa_paraphrase")
-class CommonsenseQAParaphraseTask(CommonsenseQATask):
+@register_task("gsm_symbolic_paraphrased")
+class GSM8KParaphraseTask(GSM8KSymbolicTask):
     data_path="json"
-    data_kwargs={"data_files": {"validation" : os.path.join(dir_path, "data/commonsense_qa/output.jsonl")}}
-    input_text=lambda x: f"Answer with either A, B, C, D or E.\nQuestion:\n{x['answer'][0]}\nAnswer:"
+    data_kwargs={"data_files": {"test" : os.path.join(dir_path, "data/gsm_symbolic/output.jsonl")}}
+    input_text=lambda x: x["answer"][0]
     output_text=lambda x: x["ground_truth"]
-    # sampling_args={"n": 10}
-
-def gsm8k_paraphrased_input_text(x):
-    input_text = x['answer'][0]
-    input_text.split("Question:")[-1].strip()
-    return f"Question:\n{input_text}\nAnswer:\nLet's think step by step.\n"
-
-def gsm8k_input_text(x):
-    return f"Question:\n{x['question']}\nAnswer:\nLet's think step by step.\n"
-
-SYSYTEM_MESSAGE = """\
-Let's think step by step and put the final answer in \\boxed{}.\
-"""
-
-@register_task("gsm8k_paraphrase")
-class GSM8KParaphraseTask(GSM8KTask):
-    data_path="json"
-    data_kwargs={"data_files": {"validation" : os.path.join(dir_path, "data/gsm8k/output.jsonl")}}
-    input_text=gsm8k_paraphrased_input_text
-    output_text=lambda x: x["ground_truth"]
-    system_message=SYSYTEM_MESSAGE
-    postprocessor=get_boxed_answer
-    # sampling_args={"n": 10}
-
-@register_task("gsm8k_baseline")
-class GSM8KBaseTask(GSM8KTask):
-    input_text=gsm8k_input_text
-    system_message=SYSYTEM_MESSAGE
-    postprocessor=get_boxed_answer
-
