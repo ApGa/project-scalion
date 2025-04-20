25bcc782deea763e48a07b40bdc9017835ae55e1
lindiatjuatja <lindiat@andrew.cmu.edu>
2025-04-20 00:19:27 -0400

    fix prompt format, wip pipeline

--- text-experiments/scripts/create_feedback_data.sh
+++ text-experiments/scripts/create_feedback_data.sh
@@ -21,6 +21,7 @@ MAX_ITERATIONS=$1
 MODEL=$2
 API_BASE=$3
 OUTPUT_PATH=$4
+RUN_NAME=$5
 
 # first evaluate on original questions
 # yeval \
@@ -29,15 +30,16 @@ OUTPUT_PATH=$4
 #     --trust_remote_code \
 #     --api_base $API_BASE \
 #     --output_path $OUTPUT_PATH/0 \
-#     --include_path tasks/
+#     --include_path tasks/ \
+#     --run_name $RUN_NAME
 
 mkdir -p $OUTPUT_PATH/paraphrase_input_data
 mkdir -p $OUTPUT_PATH/paraphrase_questions
 for i in $(seq 1 $MAX_ITERATIONS); do
-    model_output_path=$OUTPUT_PATH/${i-1}/output.jsonl
+    model_output_path=$OUTPUT_PATH/$((i-1))/$RUN_NAME/output.jsonl
     save_filepath=$OUTPUT_PATH/paraphrase_input_data/$i.jsonl
     # filter and create inputs for paraphrase model
-    python feedback_data.py \
+    python scripts/feedback_data.py \
         --model_output_filepath $model_output_path \
         --save_filepath $save_filepath
 
@@ -49,26 +51,27 @@ for i in $(seq 1 $MAX_ITERATIONS); do
     fi
 
     # paraphrase questions
-    data_kwargs="{'data_files': '$save_filepath'}"
+    data_kwargs='{"data_files": "'$save_filepath'"}'
     output_path=$OUTPUT_PATH/$i/paraphrase_questions
     echo $data_kwargs
     yeval \
         --model $MODEL \
         --task generate_paraphrase_with_feedback \
-        --data_kwargs $data_kwargs \
+        --data_kwargs "$data_kwargs" \
         --trust_remote_code \
         --api_base $API_BASE \
         --output_path $output_path \
         --include_path tasks/
 
-    # then evaluate the paraphrased questions
-    data_kwargs="{'data_files': '$output_path/output.jsonl'}"
+    # then evaluate the paraphrased questions    
+    data_kwargs='{"data_files": "'$output_path/output.jsonl'"}'
     yeval \
         --model $MODEL \
         --task score_paraphrase//prompt_cot \
-        --data_kwargs $data_kwargs \
+        --data_kwargs "$data_kwargs" \
         --trust_remote_code \
         --api_base $API_BASE \
         --output_path $OUTPUT_PATH/$i \
-        --include_path tasks/
+        --include_path tasks/ \
+        --run_name $RUN_NAME
 done
--- text-experiments/scripts/feedback_data.py
+++ text-experiments/scripts/feedback_data.py
@@ -10,15 +10,19 @@ import sys
 def create_input_text_with_feedback(
     output_file: str,
 ) -> pd.DataFrame:
-    def extract_question(full_input):
+    def extract_question(step):
+        full_input = step[0]["full_input"]
         content = full_input[0]["content"]
         question = content.split("\n")[1].strip()
         return question
     
     def format_input_text(x):
-        question = x["orig_question"]
-        answer = x["answer"]
-        input_text = f"This is the original question provided to the solver: {orig_question}. The solver incorrectly answered the question with the following answer: {answer}. Rephrase the question to make the problem easier to solve. Do not include the answer in your response."
+        orig_question = x["orig_question"]
+        answer = x["answer"][0]
+        input_text = f"This is the original question provided to the solver: \"{orig_question}\".\n\
+The solver incorrectly answered the question with the following answer: {answer}.\n\
+Rephrase the question to make the problem easier to solve. Do not include the answer in your response."
+        return input_text
         
     results_df = pd.read_json(output_file, lines=True)
     # filter for incorrect examples
@@ -27,7 +31,7 @@ def create_input_text_with_feedback(
         return None
     feedback_data = pd.DataFrame()
     feedback_data["idx"] = incorrect["idx"]
-    feedback_data["orig_question"] = incorrect["full_input"].apply(lambda x: extract_question(x))
+    feedback_data["orig_question"] = incorrect["step"].apply(lambda x: extract_question(x))
     feedback_data["answer"] = incorrect["answer"]
     feedback_data["ground_truth"] = incorrect["ground_truth"]
     feedback_data["input"] = feedback_data.apply(lambda x: format_input_text(x), axis=1)
