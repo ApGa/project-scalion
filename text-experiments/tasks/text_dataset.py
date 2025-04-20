8f74c1ea74fab31fc43ce5aeab0a729bced32347
lintang <lintang@sutawika.com>
2025-04-19 23:48:45 -0400

    remove unused tasks

--- text-experiments/tasks/text_dataset.py
+++ text-experiments/tasks/text_dataset.py
@@ -169,25 +169,10 @@ class ScoreParaphraseTask(YevalTask):
     user_message="Let's reason step by step and and then write the final answer within \\boxed{}."
     data_path="json"
     test_split="train"
-    # Set where the data is
-    # data_kwargs={"data_files": {"test" : os.path.join(dir_path, f"data/paraphrased/{MODEL}/gsm_symbolic_1/output.jsonl")}}
-    # data_kwargs={"data_files": os.path.join(dir_path, f"data/paraphrased/{MODEL}/gsm_symbolic_1/output.jsonl")}
     preprocessing=spread
     postprocessor=get_boxed_answer
     input_text=lambda x: x["input"]
     output_text=lambda x: x["output"]
-
-@register_task("gsm_symbolic_2_paraphrased")
-class GSM8KParaphrase2Task(GSM8KParaphrase1Task):
-    data_kwargs={"data_files": {"test" : os.path.join(dir_path, f"data/paraphrased/{MODEL.replace("/", "_")}/gsm_symbolic_2/output.jsonl")}}
-
-@register_task("gsm_symbolic_3_paraphrased")
-class GSM8KParaphrase3Task(GSM8KParaphrase1Task):
-    data_kwargs={"data_files": {"test" : os.path.join(dir_path, f"data/paraphrased/{MODEL.replace("/", "_")}/gsm_symbolic_3/output.jsonl")}}
-
-@register_task("gsm_symbolic_4_paraphrased")
-class GSM8KParaphrase4Task(GSM8KParaphrase1Task):
-    data_kwargs={"data_files": {"test" : os.path.join(dir_path, f"data/paraphrased/{MODEL.replace("/", "_")}/gsm_symbolic_4/output.jsonl")}}
 
 @register_task("gsm_symbolic_paraphrased_with_feedback")
 class GSM8KParaphrase1Task(GSM8KSymbolicTask):
