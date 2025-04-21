4f188e00f0ac2cebfc2fd3e84b57545bb714f368
lintang <lintang@sutawika.com>
2025-04-20 22:18:15 -0400

    fixed

--- text-experiments/scripts/baseline_pipeline.sh
+++ text-experiments/scripts/baseline_pipeline.sh
@@ -13,7 +13,7 @@ TASK=$1
 P_MODEL=$2
 E_MODEL=$3
 OUTPUT_MODEL_PATH=$4
-
+export NCCL_P2P_DISABLE=1
 set -a 
 source scripts/configs/.env
 set +a
@@ -27,58 +27,61 @@ PORT=4512
 for I in 1
 do
 
-    # 1. Generate Paraphrased data by model p1
-    vllm serve $P_MODEL --port ${PORT} --max_model_len 2048 > ${TMP_DIR}o.txt &
-    for NUM in {1..4}
-    do
-        RUN_NAME=${TASK}_${NUM}
-        yeval \
-            --model $P_MODEL \
-            --task ${TASK}t//generate_paraphrase_${NUM} \
-            --n_samples 250 \
-            --api_base "http://localhost:${PORT}/v1" \
-            --run_name ${RUN_NAME} \
-            --output_path tasks/data/paraphrased_train/${TASK}/${P_MODEL}/ \
-            --include_path tasks/
-    done
+    # # 1. Generate Paraphrased data by model p1
+    # vllm serve $P_MODEL --port ${PORT} --max_model_len 2048 > ${TMP_DIR}o.txt &
+    # # for NUM in {1..4}
+    # for NUM in {1..2}
+    # do
+    #     RUN_NAME=${TASK}_${NUM}
+    #     yeval \
+    #         --model $P_MODEL \
+    #         --task ${TASK}t//generate_paraphrase_${NUM} \
+    #         --n_samples 500 \
+    #         --api_base "http://localhost:${PORT}/v1" \
+    #         --run_name ${RUN_NAME} \
+    #         --output_path tasks/data/paraphrased_train/${TASK}/${P_MODEL}/ \
+    #         --include_path tasks/
+    # done
 
-    pkill vllm
-    sleep 120
+    # pkill vllm
+    # sleep 120
 
-    # 2. Evaluate how well the paraphrased data by e1,..eN
-    vllm serve $E_MODEL --port ${PORT} --max_model_len 2048 > ${TMP_DIR}o.txt &
-    for NUM in {1..4}
-    do
-        RUN_NAME=${TASK}_${NUM}
-        yeval \
-            --model $E_MODEL \
-            --task score_paraphrase \
-            --include_path tasks/ \
-            --data_kwargs "{'data_files': 'tasks/data/paraphrased_train/${TASK}/${P_MODEL}/${RUN_NAME}/output.jsonl'}" \
-            --api_base "http://localhost:${PORT}/v1" \
-            --run_name ${RUN_NAME} \
-            --output_path data/raw/${E_MODEL}
-    done
+    # # 2. Evaluate how well the paraphrased data by e1,..eN
+    # vllm serve $E_MODEL --port ${PORT} --max_model_len 2048 > ${TMP_DIR}o.txt &
+    # # for NUM in {1..4}
+    # for NUM in {1..2}
+    # do
+    #     RUN_NAME=${TASK}_${NUM}
+    #     yeval \
+    #         --model $E_MODEL \
+    #         --task score_paraphrase \
+    #         --include_path tasks/ \
+    #         --data_kwargs "{'data_files': 'tasks/data/paraphrased_train/${TASK}/${P_MODEL}/${RUN_NAME}/output.jsonl'}" \
+    #         --api_base "http://localhost:${PORT}/v1" \
+    #         --run_name ${RUN_NAME} \
+    #         --output_path data/raw/${TASK}/${E_MODEL}
+    # done
 
-    pkill vllm
-    sleep 120
+    # pkill vllm
+    # sleep 120
 
     # 3. Generate preference data
     python scripts/generate_preference_dataset.py \
-        --input_path data/raw/${E_MODEL} \
-        --output_path data/preference/${E_MODEL} \
+        --input_path data/raw/${TASK}/${E_MODEL} \
+        --output_path data/preference/${TASK}/${E_MODEL} \
         --source_path tasks/data/paraphrased_train/${TASK}/${P_MODEL}/
 
     # 4. DPO Training
     bash scripts/train-dpo.sh \
         ${E_MODEL} \
-        data/preference/${E_MODEL} \
+        data/preference/${TASK}/${E_MODEL} \
         ${OUTPUT_MODEL_PATH}
 
     # 6. Trained P Model generates paraphrased data
     TRAINED_MODEL=${OUTPUT_MODEL_PATH}/model/
     vllm serve $TRAINED_MODEL --port ${PORT} --max_model_len 2048 > ${TMP_DIR}o.txt &
-    for NUM in {1..4}
+    # for NUM in {1..4}
+    for NUM in {1..2}
     do
         RUN_NAME=${TASK}_${NUM}
         yeval \
@@ -96,7 +99,8 @@ do
 
     # 7. Evaluate how well Trained P Model is 
     vllm serve $E_MODEL --port ${PORT} --max_model_len 2048 > ${TMP_DIR}o.txt &
-    for TNUM in {1..4}
+    # for TNUM in {1..4}
+    for NUM in {1..2}
     do
         RUN_NAME=${TASK}_${TNUM}
         for NUM in {1..3}
